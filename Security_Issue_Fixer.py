4469fc115d6c161ceb48325088f64c97e8c18a97
DBRajput <itsdigvijaysing@gmail.com>
2024-02-15 02:05:54 +0530

    code for nested query

--- Security_Issue_Fixer.py
+++ Security_Issue_Fixer.py
@@ -1,8 +1,16 @@
 import re #Regular Expression
 
 def extract_soql_queries(apex_code):
-    pattern = re.compile(r'Select\s.*\sFrom\s.*\;', re.IGNORECASE | re.DOTALL)
-    queries = pattern.findall(apex_code)
+    pattern = re.compile(r'Select\s.*?\sFrom\s.*?(\s*?Where\s.*?)?;', re.IGNORECASE | re.DOTALL)
+    queries = []
+    stack = [apex_code]
+    while stack:
+        current_code = stack.pop()
+        for match in pattern.finditer(current_code):
+            query = match.group(0)
+            queries.append(query)
+            nested_code = query.split('SELECT', 1)[-1].rsplit('FROM', 1)[0]
+            stack.append(nested_code.strip())
     return queries
 
 # Example usage
@@ -11,6 +19,7 @@ Account[] accounts = [SELECT Id, Name FROM Account WHERE CreatedDate = TODAY];
 Contact[] contacts = [SELECT Id, Name FROM Contact 
                       WHERE AccountId IN :accountIds];
 asdvasdg
+Account[] accounts = [SELECT Id, Name, (SELECT Id, Name FROM Contacts) FROM Account WHERE CreatedDate = TODAY];
 """
 soql_queries = extract_soql_queries(apex_code)
 for query in soql_queries:
