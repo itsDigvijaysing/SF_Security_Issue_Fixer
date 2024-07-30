5bc48bd4317d53cb12bcc8e66afb51a2f9db2221
DBRajput <itsdigvijaysing@gmail.com>
2024-07-30 15:59:54 +0530

    Sample Code of Connecting flask and React

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/members")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Hello World </h1>
+      <h2> {JSON.stringify(data)} </h2>
     </div>
   );
 }
